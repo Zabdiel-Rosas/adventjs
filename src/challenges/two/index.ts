60ce2e8bcff399975faee18251e7297710471470
Zabdiel-Rosas <zabdiel_rosas@outlook.com>
2023-09-27 14:07:49 -0600

    refactor(challenges/two): refactor code

--- src/challenges/two/index.ts
+++ src/challenges/two/index.ts
@@ -3,18 +3,15 @@ export const holidays = ['01/06', '04/01', '12/25']
 
 export function countHours(year: number, holidays: string[]) {
   let extraHours = 0
-  const stringDates = holidays.map((item) => {
-    return `${year}/` + item
-  })
 
-  for (let item of stringDates) {
-    const date = new Date(item)
+  for (let holiday of holidays) {
+    const date = new Date(`${year}/${holiday}`)
     const day = date.getDay()
 
     if (day > 0 && day < 6) {
       extraHours += 2
     }
   }
-  // return extra hours
+
   return extraHours
 }
